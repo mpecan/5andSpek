8fd9a082133ab6d2b2dedd50df32a9e3cd8b4110
Matjaz Domen Pecan <matjaz.pecan@affinitas.de>
2017-08-15 17:33:44 +0200

    Initial commit

---
+++ src/main/kotlin/si/pecan/five/and/spek/Application.kt
@@ -0,0 +1,11 @@
+package si.pecan.five.and.spek
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
